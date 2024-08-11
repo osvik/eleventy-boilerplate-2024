ad40bff45be4b0ff687b4695d243735dda90f1d0
Osvaldo Gago <opgago@yahoo.com>
2024-08-11 18:48:14 +0100

    More changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,32 @@
-/* jshint esversion:6 */
-
+// Import the clean-css library to minify CSS
 const CleanCSS = require("clean-css");
 
+// Export a function that takes the Eleventy configuration object as a parameter
 module.exports = (function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("assets");
-	eleventyConfig.addPassthroughCopy("favicon.ico");
-	eleventyConfig.ignores.add("README.md");
-	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-	eleventyConfig.addFilter("cssmin", function (code) {
-		return new CleanCSS({}).minify(code).styles;
-	});
-	return {
-		dir: {
-			output: "docs",
-			layouts: "_layouts",
-		}
-	};
+    // Add the 'assets' directory to the list of files to copy to the output directory
+    eleventyConfig.addPassthroughCopy("assets");
+
+    // Add the 'favicon.ico' file to the list of files to copy to the output directory
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+
+    // Ignore the 'README.md' file when generating the site
+    eleventyConfig.ignores.add("README.md");
+
+    // Add a shortcode for inserting the current year into a template
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    // Add a filter for minifying CSS code using the clean-css library
+    eleventyConfig.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    // Return an object with configuration options for Eleventy
+    return {
+        dir: {
+            // Set the output directory to 'docs'
+            output: "docs",
+            // Set the layouts directory to '_layouts'
+            layouts: "_layouts",
+        }
+    };
 });
